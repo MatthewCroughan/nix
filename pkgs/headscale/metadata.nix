c19fdfb1c0576e4d86523adf8788320edee4ad09
Cole Botkens <cole.mickens+colebot@gmail.com>
2021-05-01 02:18:09 +0000

    auto-update(GHA-Update-801265375-1): headscale: 51139afa50e14aa339c7bb4373d93548f7381d85 => 8198d2f8edc42b01d78c16cc4042c0ee55f7af56

--- pkgs/headscale/metadata.nix
+++ pkgs/headscale/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/juanfont/headscale";
   branch = "main";
-  rev = "51139afa50e14aa339c7bb4373d93548f7381d85";
-  sha256 = "sha256-GHqXzqq3uMftzQq7yl9uZuQEAlZeUm8HniuSqEad+MQ=";
-  vendorSha256 = "sha256-GHqXzqq3uMftzQq7yl9uZuQEAlZeUm8HniuSqEad+MQ=";
+  rev = "8198d2f8edc42b01d78c16cc4042c0ee55f7af56";
+  sha256 = "sha256-H+3B1AjpsMmVZTCT88Rs7a3qs1a4RO3AmXHPZC3bpz8=";
+  vendorSha256 = "sha256-H+3B1AjpsMmVZTCT88Rs7a3qs1a4RO3AmXHPZC3bpz8=";
 }
