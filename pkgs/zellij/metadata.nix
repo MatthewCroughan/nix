f638c312baa064e9099443f096c168fab805d6a7
Cole Botkens <cole.mickens+colebot@gmail.com>
2021-05-01 02:19:39 +0000

    auto-update(GHA-Update-801265375-1): zellij: b5d38e95c876f231a1c52ab2bcf805f65958d56c => 325fc5620f07412381dd47dcf7cf9e141a3d66bf

--- pkgs/zellij/metadata.nix
+++ pkgs/zellij/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://github.com/zellij-org/zellij";
   branch = "main";
-  rev = "b5d38e95c876f231a1c52ab2bcf805f65958d56c";
-  sha256 = "sha256-bo5R0zSGHBMlQe6EIiGI2dSty5uQzes89AULseyzuyc=";
-  cargoSha256 = "sha256-BYw7rDDXbqoj40/So8BbMXkucLIRXqtUmm2YIz0lR3o=";
+  rev = "325fc5620f07412381dd47dcf7cf9e141a3d66bf";
+  sha256 = "sha256-iYhMrjeU7VQdrTtHdNw0JdE5JJ0v5WcngKyOsaa6pPA=";
+  cargoSha256 = "sha256-Z+2DX/pLmsobybsgIxx+MvvYpF2LI5QdfcO5uCcepkM=";
 }
